80de572940f83a34af48436d73e4faf0544f327d
Anup Raj Niroula <a.r.niroula@gmail.com>
2026-05-08 20:29:35 -0400

    Fix path

--- scripts/s3_vector.py
+++ scripts/s3_vector.py
@@ -8,7 +8,7 @@ load_dotenv()
 REGION = "us-east-1"
 VECTOR_BUCKET = "uni-rec-s3-vector-bucket"
 INDEX_NAME = "uni-rec-index"
-NPY_FILE = "../rag/reviews/embeddings.npy"
+NPY_FILE = "rag/reviews/embeddings.npy"
 ADD_METADATA = True
 BATCH_SIZE = 500
 
@@ -23,10 +23,7 @@ num_vectors, dimension = embeddings.shape
 print(f"Loaded {num_vectors} vectors")
 print(f"Dimension: {dimension}")
 
-s3vectors = boto3.client(
-    "s3vectors",
-    region_name=REGION
-)
+s3vectors = boto3.client("s3vectors", region_name=REGION)
 
 for start in tqdm(range(0, num_vectors, BATCH_SIZE)):
     end = min(start + BATCH_SIZE, num_vectors)
@@ -34,25 +31,16 @@ for start in tqdm(range(0, num_vectors, BATCH_SIZE)):
     batch = []
 
     for i in range(start, end):
-        vector = {
-            "key": f"vec-{i}",
-            "data": {
-                "float32": embeddings[i].tolist()
-            }
-        }
+        vector = {"key": f"vec-{i}", "data": {"float32": embeddings[i].tolist()}}
 
         if ADD_METADATA:
-            vector["metadata"] = {
-                "source": "reviews",
-                "row_id": str(i)
-            }
+            vector["metadata"] = {"source": "reviews", "row_id": str(i)}
 
         batch.append(vector)
 
     response = s3vectors.put_vectors(
-        vectorBucketName=VECTOR_BUCKET,
-        indexName=INDEX_NAME,
-        vectors=batch
+        vectorBucketName=VECTOR_BUCKET, indexName=INDEX_NAME, vectors=batch
     )
 
 print("Upload complete.")
+
