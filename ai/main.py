44d27f6b2daba2734a84e64fc4776fdfbca017cb
sansha521 <yumikot2319@gmail.com>
2026-05-21 18:31:16 -0400

    fastapi logic

--- ai/main.py
+++ ai/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class UserPromptModel(BaseModel):
+    user_prompt: str
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/prompt/")
+async def create_item(user_prompt: UserPromptModel):
+    return user_prompt
