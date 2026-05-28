fdce090efb44331da97aa9232dfa87c9f107548c
sansha521 <yumikot2319@gmail.com>
2026-05-28 15:14:11 -0400

    add layer1 filtering system

---
+++ ml_model/layer1.py
@@ -0,0 +1,63 @@
+"""
+filter universities based on minimum requirements.
+Args: budget (range cost of attendance, tuition), concentration, degree (undergraduate, graduate, associate), grades (ACT, SAT), region
+Output: Top 20 universities that satisfy the requirements
+"""
+import pandas as pd
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent / "datasets/us_uni_data_filtered.csv"
+
+def load_local_data(path):
+    return pd.read_csv(path)
+
+def filter_uni(
+        budget_range: tuple[int, int],
+        score_type: str | None,
+        score: int | None,
+        region: list[str] | None,
+        data: pd.DataFrame,
+) -> pd.DataFrame:
+    
+    # filter by budget
+    min_budget, max_budget = budget_range
+    df_budget = data[(data["latest.cost.attendance.academic_year"] >= min_budget) & (data["latest.cost.attendance.academic_year"] <= max_budget)]
+
+    # filter by score
+    score_col = None
+    df_score = df_budget
+
+    if score_type == "ACT":
+        score_col = "latest.admissions.act_scores.midpoint.cumulative"
+    elif score_type == "SAT":
+        score_col = "latest.admissions.sat_scores.average.overall"
+
+    if score:
+        df_score = df_budget[df_budget[score_col] <= score]
+    
+    # filter by region
+    # TODO: add region column in dataset
+    # northeast, midatlantic, midwest, west, southeast, southwest, northwest
+    df_region = df_score
+
+    if region:
+        df_region = df_region[df_region["region"].isin(region)]
+
+    final_df = df_region
+    return final_df
+
+def layer1(
+        budget_range,
+        score_type,
+        score,
+        region,
+):
+    df = load_local_data(DATA_PATH)
+    df_filtered = filter_uni(
+        budget_range,
+        score_type,
+        score,
+        region,
+        df
+    )
+    return df_filtered
