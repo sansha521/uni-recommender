96e7fb3384c5b9de5f3d37ed648f188ec89fb0db
Anup Raj Niroula <a.r.niroula@gmail.com>
2026-06-18 17:08:33 -0400

    Modify Frontend to be completely static rendered

--- fe/frontend/astro.config.mjs
+++ fe/frontend/astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: node({
-        mode: 'standalone',
-    }),
-    output: 'server',
+    output: 'static',
     env: {
         schema: {
             API_URL: envField.string({ context: "client", access: "public", default: "http://localhost:8000/" })
